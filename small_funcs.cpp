f23cf1f6b1b2fb755a436eacef458bf9d64874a1
Andrew <30913007+ajcook14@users.noreply.github.com>
2024-07-24 09:42:42 +1000

    fixing compiler warnings

--- small_funcs.cpp
+++ small_funcs.cpp
@@ -1,9 +1,6 @@
 #include "capd/capdlib.h"
 #include "small_funcs.h"
 
-#include <iostream>
-#include <fstream>
-
 using namespace std;
 using namespace capd;
 
@@ -11,43 +8,35 @@ using namespace capd;
 
 double min_dim(IVector& x)
 {
-
     double min_dim = widths(x)[0];
-
-    for (int i = 0; i < x.dimension(); i++) {
-
-        if ( widths(x)[i] < min_dim ) min_dim = widths(x)[i];
-
+    for (std::size_t i = 0; i < x.dimension(); i++) {
+        if ( widths(x)[i] < min_dim ) {
+            min_dim = widths(x)[i];
+        }
     }
 
     return(min_dim);
-
 }
 
-int arg_max_dim(IVector& x)
+unsigned int arg_max_dim(IVector& x)
 {
+    unsigned int index_max_dim = 0;
 
-    int index_max_dim = 0;
-
-    for (int i = 0; i < x.dimension(); i++) {
-
-        if ( widths(x)[i] > widths(x)[index_max_dim] ) index_max_dim = i;
-
+    for (std::size_t i = 0; i < x.dimension(); i++) {
+        if ( widths(x)[i] > widths(x)[index_max_dim] ) {
+            index_max_dim = i;
+        }
     }
-
     return(index_max_dim);
-
 }
 
 void subdivide(IVector& x, IVector& left, IVector& right)
 {
-
     left = x;
     right = x;
 
-    int max_dim = arg_max_dim(x);
+    unsigned int max_dim = arg_max_dim(x);
 
     left[max_dim] = interval(x[max_dim].leftBound(), mid(x[max_dim]).rightBound());
     right[max_dim] = interval(mid(x[max_dim]).leftBound(), x[max_dim].rightBound());
-
 }
--- small_funcs.h
+++ small_funcs.h
@@ -5,7 +5,7 @@ using namespace capd;
 
 double min_dim(IVector& x);
 
-int arg_max_dim(IVector& x);
+unsigned int arg_max_dim(IVector& x);
 
 void subdivide(IVector& x, IVector& left, IVector& right);
 
